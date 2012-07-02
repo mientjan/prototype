1b621e5cfbc22d6b1c29f0c5a261ebe664001bad
Mient-jan Stelling <mientjan.stelling@gmail.com>
2012-07-02 22:17:29 +0200

    first proto/type thx to Jonas Wagner for his great blog post about normal mapping

---
+++ inc/js/default.js
@@ -0,0 +1,57 @@
+var Proto = new Class({
+	
+	'count':0,
+	
+	'dirpath':'proto/type{count}/',
+	'file':'icon.jpg',
+	
+	'initialize':function(ul){
+		this.ul = ul;
+		
+		this.getItem();
+	},
+	
+	'getItem':function(){
+		this.img = new Element('img', {
+			'styles':{
+				'width':40,
+				'height':40
+			}
+		});
+		this.img.onload = function(e){
+			
+			var currentCount = this.count;
+			var self = this;
+			var a = new Element('a', {
+				'events':{
+					'click':function(e){
+						$('content').empty();
+						this.getElements('! ! li a').removeClass('active');
+						this.addClass('active');
+						var iframe = new IFrame({
+							'src':self.dirpath.substitute({count:currentCount}),
+							'styles':{
+								'border':0,
+								'width':window.getSize().x,
+								'height':window.getSize().y-50
+							}
+						});
+						iframe.inject($('content'));
+					}
+				}
+			});
+			var li = new Element('li');
+			
+			this.img.inject(a);
+			a.inject(this.ul);
+			li.inject(this.ul, 'top');
+			
+			this.count++;
+			this.getItem();
+			
+		}.bind(this);
+		
+		
+		this.img.src = this.dirpath.substitute({'count':this.count}) + this.file;
+	}
+});
